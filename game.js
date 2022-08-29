58a132989f1e71db18bca5c027ed4eae140da38e
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-29 07:02:52 +0100

    added utils

---
+++ game.js
@@ -0,0 +1,45 @@
+function new2dCanvas(id, width, height) {
+  const canvas = document.getElementById(id);
+  const ctx = canvas.getContext("2d");
+  canvas.width = width;
+  canvas.height = height;
+  return [canvas, ctx];
+}
+
+function drawText(text, font, fillStyle, x, y, maxWidth = undefined) {
+  if (font) ctx.font = font;
+  if (fillStyle) ctx.fillStyle = fillStyle;
+  ctx.fillText(text, x, y, maxWidth);
+}
+
+function randUpTo(num, floor = false) {
+  const res = Math.random() * num;
+  return floor ? Math.floor(res) : res;
+}
+
+function isCircleRectColliding(circle, rect) {
+  const distX = Math.abs(circle.x - rect.x - rect.w / 2);
+  const distY = Math.abs(circle.y - rect.y - rect.h / 2);
+  if (distX > rect.w / 2 + circle.r) return false;
+  if (distY > rect.h / 2 + circle.r) return false;
+  if (distX <= rect.w / 2) return true;
+  if (distY <= rect.h / 2) return true;
+  const dx = distX - rect.w / 2;
+  const dy = distY - rect.h / 2;
+  return dx * dx + dy * dy <= circle.r * circle.r;
+}
+
+function isRectRectColliding(first, second) {
+  if (!first || !second) return false;
+  if (
+    !(
+      first.x > second.x + second.w ||
+      first.x + first.w < second.x ||
+      first.y > second.y + second.h ||
+      first.y + first.h < second.y
+    )
+  ) {
+    return true;
+  }
+  return false;
+}
